ba03df8aca352629a42defa0e80c00881f5af5f4
Yash Yadav <yashyadav.sr@gmail.com>
2025-06-24 20:43:00 +0530

    feat: added event API support (Client only)

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,24 +1,62 @@
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 
+// function download(filename, text) {
+//   var element = document.createElement('a');
+//   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+//   element.setAttribute('download', filename);
+
+//   element.style.display = 'none';
+//   document.body.appendChild(element);
+
+//   element.click();
+
+//   document.body.removeChild(element);
+// }
+
+async function fetchHolidays(country, year = 2025) {
+  // Get the window.CONFIG.API_KEY from https://calendarific.com/
+  // const response = await fetch(window.CONFIG.API_ENDPOINT + "?"  + new URLSearchParams({
+  //     api_key: window.CONFIG.API_KEY,
+  //     country: country,
+  //     year: year
+  // }) );
+
+  // const responseJson = await response.json();
+
+  // Read responseJson from testData/response.json
+  const response = await fetch("testData/response.json");
+  const responseJson = await response.json();
+
+  // const responseJson = {
+  //   foo: "bar"
+  // }
+
+  console.log(responseJson);
+  
+}
+
 addOnUISdk.ready.then(async () => {
-    console.log("addOnUISdk is ready for use.");
+  console.log("addOnUISdk is ready for use.");
 
-    // Get the UI runtime.
-    const { runtime } = addOnUISdk.instance;
+  // Get the UI runtime.
+  const { runtime } = addOnUISdk.instance;
 
-    // Get the proxy object, which is required
-    // to call the APIs defined in the Document Sandbox runtime
-    // i.e., in the `code.js` file of this add-on.
-    const sandboxProxy = await runtime.apiProxy("documentSandbox");
+  // Get the proxy object, which is required
+  // to call the APIs defined in the Document Sandbox runtime
+  // i.e., in the `code.js` file of this add-on.
+  const sandboxProxy = await runtime.apiProxy("documentSandbox");
 
-    const createRectangleButton = document.getElementById("createRectangle");
-    createRectangleButton.addEventListener("click", async event => {
-        await sandboxProxy.createRectangle();
-    });
+  const createRectangleButton = document.getElementById("createRectangle");
+  createRectangleButton.addEventListener("click", async (event) => {
+    await sandboxProxy.createRectangle();
+    // console.log(apiKey)
+    await fetchHolidays("IN");
+    // download("hello.txt","This is the content of my file :)");
+  });
 
-    // Enable the button only when:
-    // 1. `addOnUISdk` is ready,
-    // 2. `sandboxProxy` is available, and
-    // 3. `click` event listener is registered.
-    createRectangleButton.disabled = false;
+  // Enable the button only when:
+  // 1. `addOnUISdk` is ready,
+  // 2. `sandboxProxy` is available, and
+  // 3. `click` event listener is registered.
+  createRectangleButton.disabled = false;
 });
